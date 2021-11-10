589a228a77d8b6b333faaf9945f0a45714a3c937
Anders Lekve Brandseth <ALB@teknotherm.no>
2021-11-10 19:03:14 +0100

    added pollresults as child element updated ui

--- src/Client/Pages/PollManagement/PollQuestions.razor.cs
+++ src/Client/Pages/PollManagement/PollQuestions.razor.cs
@@ -16,61 +16,97 @@ using VoteApp.Application.Features.PollQuestions.Queries.GetAll;
 
 namespace VoteApp.Client.Pages.PollManagement
 {
+    public class DummyPollResult
+    {
+        public int GreenVotes { get; set; }
+        public int RedVotes { get; set; }
+        public DateTime Started { get; set; }
+        public DateTime Stopped { get; set; }
+
+    }
     public partial class PollQuestions
     {
         [Inject] private IPollQuestionManager PollQuestionManager { get; set; }
 
-    [CascadingParameter] private HubConnection HubConnection { get; set; }
+        [CascadingParameter] private HubConnection HubConnection { get; set; }
 
-    private List<GetAllPollQuestionsResponse> _pollQuestionList = new();
-    private GetAllPollQuestionsResponse _pollQuestion = new();
-    private string _searchString = "";
-    private bool _dense = false;
-    private bool _striped = true;
-    private bool _bordered = false;
+        private List<GetAllPollQuestionsResponse> _pollQuestionList = new();
+        private GetAllPollQuestionsResponse _pollQuestion = new();
+        private string _searchString = "";
+        private bool _dense = false;
+        private bool _striped = true;
+        private bool _bordered = false;
 
-    private ClaimsPrincipal _currentUser;
-    private bool _canCreateBrands;
-    private bool _canEditBrands;
-    private bool _canDeleteBrands;
-    private bool _canExportBrands;
-    private bool _canSearchBrands;
-    private bool _loaded;
+        private ClaimsPrincipal _currentUser;
+        private bool _canCreateBrands;
+        private bool _canEditBrands;
+        private bool _canDeleteBrands;
+        private bool _canExportBrands;
+        private bool _canSearchBrands;
+        private bool _loaded;
 
-    protected override async Task OnInitializedAsync()
-    {
+        private GetAllPollQuestionsResponse _selectedQuestion;
+
+        private readonly List<DummyPollResult> _selectedPollResults = new();
+
+        protected override async Task OnInitializedAsync()
+        {
             //todo: create permission for pollquestions
-        _currentUser = await _authenticationManager.CurrentUser();
-        _canCreateBrands = (await _authorizationService.AuthorizeAsync(_currentUser, Permissions.Brands.Create)).Succeeded;
-        _canEditBrands = (await _authorizationService.AuthorizeAsync(_currentUser, Permissions.Brands.Edit)).Succeeded;
-        _canDeleteBrands = (await _authorizationService.AuthorizeAsync(_currentUser, Permissions.Brands.Delete)).Succeeded;
-        _canExportBrands = (await _authorizationService.AuthorizeAsync(_currentUser, Permissions.Brands.Export)).Succeeded;
-        _canSearchBrands = (await _authorizationService.AuthorizeAsync(_currentUser, Permissions.Brands.Search)).Succeeded;
+            _currentUser = await _authenticationManager.CurrentUser();
+            _canCreateBrands = (await _authorizationService.AuthorizeAsync(_currentUser, Permissions.Brands.Create)).Succeeded;
+            _canEditBrands = (await _authorizationService.AuthorizeAsync(_currentUser, Permissions.Brands.Edit)).Succeeded;
+            _canDeleteBrands = (await _authorizationService.AuthorizeAsync(_currentUser, Permissions.Brands.Delete)).Succeeded;
+            _canExportBrands = (await _authorizationService.AuthorizeAsync(_currentUser, Permissions.Brands.Export)).Succeeded;
+            _canSearchBrands = (await _authorizationService.AuthorizeAsync(_currentUser, Permissions.Brands.Search)).Succeeded;
 
-        await GetPollQuestionsAsync();
-        _loaded = true;
-        HubConnection = HubConnection.TryInitialize(_navigationManager);
-        if (HubConnection.State == HubConnectionState.Disconnected)
-        {
-            await HubConnection.StartAsync();
-        }
-    }
-
-    private async Task GetPollQuestionsAsync()
-    {
-        var response = await PollQuestionManager.GetAllAsync();
-        if (response.Succeeded)
-        {
-            _pollQuestionList = response.Data.ToList();
-        }
-        else
-        {
-            foreach (var message in response.Messages)
+            await GetPollQuestionsAsync();
+            _loaded = true;
+            HubConnection = HubConnection.TryInitialize(_navigationManager);
+            if (HubConnection.State == HubConnectionState.Disconnected)
             {
-                _snackBar.Add(message, Severity.Error);
+                await HubConnection.StartAsync();
+            }
+
+            _selectedPollResults.Add(new DummyPollResult()
+            {
+                GreenVotes = 10,
+                RedVotes = 11,
+                Started = DateTime.Now,
+                Stopped = DateTime.Now
+            });
+
+            _selectedPollResults.Add(new DummyPollResult()
+            {
+                GreenVotes = 15,
+                RedVotes = 9,
+                Started = DateTime.Now,
+                Stopped = DateTime.Now
+            });
+
+            _selectedPollResults.Add(new DummyPollResult()
+            {
+                GreenVotes = 23,
+                RedVotes = 27,
+                Started = DateTime.Now,
+                Stopped = DateTime.Now
+            });
+        }
+
+        private async Task GetPollQuestionsAsync()
+        {
+            var response = await PollQuestionManager.GetAllAsync();
+            if (response.Succeeded)
+            {
+                _pollQuestionList = response.Data.ToList();
+            }
+            else
+            {
+                foreach (var message in response.Messages)
+                {
+                    _snackBar.Add(message, Severity.Error);
+                }
             }
         }
-    }
 
         //private async Task Delete(int id)
         //{
@@ -116,19 +152,31 @@ namespace VoteApp.Client.Pages.PollManagement
         }
 
         private async Task Reset()
-    {
-        _pollQuestion = new GetAllPollQuestionsResponse();
-        await GetPollQuestionsAsync();
-    }
-
-    private bool Search(GetAllPollQuestionsResponse pollQuestion)
-    {
-        if (string.IsNullOrWhiteSpace(_searchString)) return true;
-        if (pollQuestion.Title?.Contains(_searchString, StringComparison.OrdinalIgnoreCase) == true)
         {
-            return true;
+            _pollQuestion = new GetAllPollQuestionsResponse();
+            await GetPollQuestionsAsync();
         }
-        return false;
+
+        private bool Search(GetAllPollQuestionsResponse pollQuestion)
+        {
+            if (string.IsNullOrWhiteSpace(_searchString)) return true;
+            if (pollQuestion.Title?.Contains(_searchString, StringComparison.OrdinalIgnoreCase) == true)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void OnSelectedQuestion(TableRowClickEventArgs<GetAllPollQuestionsResponse> pollQuestion)
+        {
+            if (_selectedQuestion == pollQuestion.Item)
+            {
+                _selectedQuestion = null;
+            }
+            else
+            {
+                _selectedQuestion = pollQuestion.Item;
+            }
+        }
     }
-}
 }

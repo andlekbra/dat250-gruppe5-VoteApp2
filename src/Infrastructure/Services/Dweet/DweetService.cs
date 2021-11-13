2fea046d90ca52132ea900adb5beb1b3b1af741b
andlekbra <89541859+andlekbra@users.noreply.github.com>
2021-11-13 14:26:50 +0100

    Added abstraction (#10)
    Co-authored-by: Anders Lekve Brandseth <ALB@teknotherm.no>

--- src/Infrastructure/Services/Dweet/DweetService.cs
+++ src/Infrastructure/Services/Dweet/DweetService.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VoteApp.Application.Interfaces.Services;
+using VoteApp.Application.Models.PollStartNotification;
+using VoteApp.Application.Models.PollStopNotification;
 
 namespace VoteApp.Infrastructure.Services.Dweet
 {
-    class DweetService : IDweetService
+    class DweetService : IPollStartNotificationService, IPollStopNotificationService
     {
+        public void Notify(PollStartNotificationMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Notify(PollStopNotificationMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task SendAsync()
         {
             throw new NotImplementedException();
